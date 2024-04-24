dd76d0224d70269ef3efab628c61cbca0909bc5c
salsdev <salehaliyu111@gmail.com>
2024-04-24 00:45:01 +0000

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Werkzeug and Flask from pip3 using Puppet
+
+package { 'Werkzeug':
+    ensure   => '2.2.2',
+    provider => 'pip3',
 }
 
+package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
